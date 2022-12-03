80cd25eeebf2875b74328d56a82f7a4d23bab2da
Viktor Markov <markov.viktor.an@yandex.ru>
2022-12-03 15:24:33 +0300

    begin

---
+++ src/components/App/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import Header from "../Header/Header";
+
+function App() {
+
+  // function a() {
+
+  // }
+
+  return (
+    <div className="App">
+      <Header></Header>
+        {/* <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+
+          {loggedIn && (
+            <ProtectedRoute
+            />
+          )}
+
+          {loggedIn && (
+            <ProtectedRoute
+            />
+          )}
+
+          {loggedIn && (
+            <ProtectedRoute
+            />
+          )}
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <p>HeaderElement</p>
+    </header>
+  );
+}
+
+export default Header;
