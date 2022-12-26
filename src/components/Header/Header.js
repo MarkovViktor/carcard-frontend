c65eabb6277c9dd93caea65d4d3b55b18dbe97bf
Viktor Markov <markov.viktor.an@yandex.ru>
2022-12-26 11:22:28 +0300

    heder_img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import "./Header.css";
 import headerImg from '../../images/header-img.jpg';
+import bannerImg from '../../images/banner-img.png';
 
 function Header() {
   return (
     <header className="Header">
       <img className="header__bg-img" src={headerImg} alt="Фон из облаков" />
-      <div className="header__banner">  
-        <div>
-          <h1 className="header__title">АВТОМОБИЛЬНАЯ ВИЗИТКА </h1>
+      <div className="header__banner">
+        <img className="header__banner_img" src={bannerImg} alt="Автовизитка" />  
+        <div className="header__banner_buy">
+          <h1 className="header__title">АВТОВИЗИТКА </h1>
           <a href="" className="header__banner_button">Изготовить</a>
         </div>
       </div>
