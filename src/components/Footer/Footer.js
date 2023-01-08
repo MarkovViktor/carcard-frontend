91bb194770545b44b3c457feda6519a6139a7d32
Viktor Markov <markov.viktor.an@yandex.ru>
2023-01-08 22:49:58 +0300

    add_navigaton

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="Footer">
-      <a target='blank' href="https://github.com/MarkovViktor" className="footer__title" >markov viktor</a>
+      <a target='blank' href="https://github.com/MarkovViktor" className="footer__title" >Powered by Markov Viktor</a>
     </footer>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import "./Header.css";
 import headerImg from '../../images/header-img.jpg';
 import bannerImg from '../../images/banner-img.png';
+import headerLogo from '../../images/header-logo.jpg';
 
 function Header() {
   return (
     <header className="Header">
       <img className="header__bg-img" src={headerImg} alt="Фон сайта" />
+      <nav className="header__nav">
+          <img className="header__logo_img" src={headerLogo} alt="Логотип" />
+          <ul>
+            <li>
+              <a>Главная</a>
+            </li>
+            <li>
+              <a>Производство</a>
+            </li>
+            <li>
+              <a>Заказать</a>
+            </li>
+          </ul>
+      </nav>  
       <div className="header__banner">
         <img className="header__banner_img" src={bannerImg} alt="Автовизитка" />  
         <div className="header__banner_buy">
